2db9d123d7905822f218f126b5c889bd27ed819b
Samarth Bhutani <samarth.bhutani@berkeley.edu>
2022-05-11 02:33:29 +0000

    fixed tests

--- traffic_tools/tests/test_utils.py
+++ traffic_tools/tests/test_utils.py
@@ -5,22 +5,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def test1():
-    assert encode_weekend(5) == True
-    assert encode_weekend(6) == True
-    assert encode_weekend(7) == False
+    assert utils.encode_weekend(5) == True
+    assert utils.encode_weekend(6) == True
+    assert utils.encode_weekend(7) == False
     
 def test2():
-    assert encode_rain_level(0.4) == 'No rain'
-    assert encode_rain_level(1) == 'Weak rain'
-    assert encode_rain_level(4) == 'Moderate Rain'
-    assert encode_rain_level(7) == 'Heavy Rain'
-    assert encode_rain_level(11) == 'Extreme Rain'
+    assert utils.encode_rain_level(0.4) == 'No rain'
+    assert utils.encode_rain_level(1) == 'Weak rain'
+    assert utils.encode_rain_level(4) == 'Moderate Rain'
+    assert utils.encode_rain_level(7) == 'Heavy Rain'
+    assert utils.encode_rain_level(11) == 'Extreme Rain'
 
 def test3():
-    assert encode_snow_level(0) == 'No snow'
-    assert encode_snow_level(0.24) == 'Small snow'
-    assert encode_snow_level(5) == 'Big snow'
+    assert utils.encode_snow_level(0) == 'No snow'
+    assert utils.encode_snow_level(0.24) == 'Small snow'
+    assert utils.encode_snow_level(5) == 'Big snow'
 
 def test4():
-    assert encode_holiday('None') == False
-    assert encode_holiday('None12') == True
+    assert utils.encode_holiday('None') == False
+    assert utils.encode_holiday('None12') == True
